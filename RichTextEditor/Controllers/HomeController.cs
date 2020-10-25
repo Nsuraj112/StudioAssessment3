7fc817fdd42b3c83e5b626520cbd3e0d910fd26c
Nsuraj112 <snag0018@student.monash.edu>
2020-10-26 02:40:29 +0530

    Add project files.

---
+++ RichTextEditor/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Helpers;
+
+
+namespace RichTextEditor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SendMail()
+        {
+            string recipient = Request["to"];
+            string subject = Request["subject"];
+            string body = Request["body"];
+
+            WebMail.SmtpServer = "smtp.gmail.com";
+            WebMail.SmtpPort = 587;
+            WebMail.SmtpUseDefaultCredentials = true;
+            WebMail.EnableSsl = true;
+            WebMail.UserName = "Email address";
+            WebMail.Password = "Password";
+
+            WebMail.Send(to: recipient, subject: subject, body: body, isBodyHtml: true);
+
+            return View();
+        }
+    }
+}
